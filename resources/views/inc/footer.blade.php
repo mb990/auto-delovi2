4f2fb34f1a4a1020b4be43c36cdeef924cbfdbc3
mb990 <stayforff@gmail.com>
2018-11-11 20:29:35 +0100

    popravljen proizvodi meni

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -4,7 +4,7 @@
                 <h2 class="text-center">Гдје се налазимо</h2>
                 <p class="text-center">СТЕФАНА ДЕЧАНСКОГ 131, 76000 БИЈЕЉИНА</p>
             </div>
-        </div><iframe allowfullscreen="" frameborder="0" width="100%" height="450" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2833.082695938464!2d19.195488215533988!3d44.75872817909897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475be8d9ea7c5217%3A0x30205130403c35f9!2z0KHRgtC10YTQsNC90LAg0JTQtdGH0LDQvdGB0LrQvtCzIDEzMSwg0JHQuNGY0LXRmdC40L3QsCA3NjMwMCwg0JHQvtGB0L3QsCDQuCDQpdC10YDRhtC10LPQvtCy0LjQvdCw!5e0!3m2!1ssr!2srs!4v1541890333545" style="width:400PX;height:300PX;margin:0px;"></iframe>
+        </div><iframe allowfullscreen="" frameborder="0" width="100%" height="450" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2833.082695938464!2d19.195488215533988!3d44.75872817909897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475be8d9ea7c5217%3A0x30205130403c35f9!2z0KHRgtC10YTQsNC90LAg0JTQtdGH0LDQvdGB0LrQvtCzIDEzMSwg0JHQuNGY0LXRmdC40L3QsCA3NjMwMCwg0JHQvtGB0L3QsCDQuCDQpdC10YDRhtC10LPQvtCy0LjQvdCw!5e0!3m2!1ssr!2srs!4v1541890333545" style="width:300PX;height:225PX;margin:0px;"></iframe>
         <footer>
             <ul class="list-inline">
                 <li class="list-inline-item"><a href="/">Почетна</a></li>
