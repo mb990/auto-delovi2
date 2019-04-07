5085b8529c7f257f0f217ea59fd2189587d51966
mb990 <stayforff@gmailcom>
2019-04-07 14:00:42 +0200

    small proizvodi change

--- resources/views/proizvodi.blade.php
+++ resources/views/proizvodi.blade.php
@@ -10,15 +10,36 @@
             @section('content')
             <h2 class="text-center">@section('heading') <?php echo $heading ?> @endsection</h2>
 
-               
+             <div class="container">
+             	<div class="row">
+             		<div class="col-md-4">
+             			<ul style="padding:48px; ">
+		                    <li>Уља</li>
+		                    <li>Акумулатори</li>
+		                    <li>Ауто Гуме</li>
+		                    <li>Остали ауто-дијелови</li>
+	                	</ul>
+            		</div>
+            		<div class="col-md-4">
+             			<ul style="padding:48px; ">
+		                    <li>Уља</li>
+		                    <li>Акумулатори</li>
+		                    <li>Ауто Гуме</li>
+		                    <li>Остали ауто-дијелови</li>
+	                	</ul>
+            		</div>
+            		<div class="col-md-4">
+             			<ul style="padding:48px; ">
+		                    <li>Уља</li>
+		                    <li>Акумулатори</li>
+		                    <li>Ауто Гуме</li>
+		                    <li>Остали ауто-дијелови</li>
+	                	</ul>
+            		</div>
+             	</div>
+             </div>  
 
-                <ul style="padding:48px; ">
-
-                    <li>Уља</li>
-                    <li>Акумулатори</li>
-                    <li>Ауто Гуме</li>
-                    <li>Остали ауто-дијелови</li>
-                </ul>
+                
             @endsection
        
    
